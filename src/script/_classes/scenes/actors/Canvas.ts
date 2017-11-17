1f8f2e52ed0f67ba6972fd8d42824f641abec787
codeartisticninja <the@codeartistic.ninja>
2017-11-17 21:57:56 +0100

    smear!

--- src/script/_classes/scenes/actors/Canvas.ts
+++ src/script/_classes/scenes/actors/Canvas.ts
@@ -8,31 +8,71 @@ import Vector2 from "../../lib/utils/Vector2";
  */
 
 export default class Canvas extends Actor {
-  public canvasElement: HTMLCanvasElement;
-  public context: CanvasRenderingContext2D;
+  public canvasEl: HTMLCanvasElement;
+  public canvasCtx: CanvasRenderingContext2D;
+  public brushEl: HTMLCanvasElement;
+  public brushCtx: CanvasRenderingContext2D;
+  public brushPos: Vector2 = new Vector2();
+  public brushSize: number = 64;
 
   constructor(scene: Scene, obj: any) {
     super(scene, obj);
-    this.canvasElement = document.createElement("canvas");
-    this.canvasElement.width = this.size.x;
-    this.canvasElement.height = this.size.y;
-    this.context = <CanvasRenderingContext2D>this.canvasElement.getContext("2d");
+    this.canvasEl = document.createElement("canvas");
+    this.canvasEl.width = this.size.x;
+    this.canvasEl.height = this.size.y;
+    this.canvasCtx = <CanvasRenderingContext2D>this.canvasEl.getContext("2d");
+
+    this.brushEl = document.createElement("canvas");
+    this.brushEl.width =
+      this.brushEl.height = this.brushSize;
+    this.brushCtx = <CanvasRenderingContext2D>this.brushEl.getContext("2d");
+
+    this.brushCtx.beginPath();
+    this.brushCtx.arc(this.brushSize / 2, this.brushSize / 2, this.brushSize / 2, 0, 2 * Math.PI);
+    this.brushCtx.fill();
+    this.brushCtx.globalCompositeOperation = "source-atop";
+
+    this.splat();
   }
 
   update() {
-    let g = this.context;
-    let m = this.scene.mouse;
+    let cg = this.canvasCtx;
+    let bg = this.brushCtx;
+    let mp = this.scene.mouse;
+    let bp = this.brushPos;
+    let bs = this.brushSize;
+    let d = Vector2.dispense().copyFrom(mp).subtract(bp);
     super.update();
     if (this.scene.mousePressed) {
-      g.fillStyle = "black";
-      g.fillRect(m.x - 8, m.y - 8, 16, 16);
+      while (d.magnitude > 2) {
+        d.normalize();
+        bp.add(d);
+        bg.drawImage(this.canvasEl, bp.x - bs / 2, bp.y - bs / 2, bs, bs, 0, 0, bs, bs);
+        bp.add(d);
+        cg.drawImage(this.brushEl, 0, 0, bs, bs, bp.x - bs / 2, bp.y - bs / 2, bs, bs);
+        d.copyFrom(mp).subtract(bp);
+      }
+    } else {
+      bp.copyFrom(mp);
+    }
+    if (this.scene.mouseJustPressed) {
+      cg.drawImage(this.scene.game.canvas, 0, 0);
     }
   }
 
   render() {
     let g = this.scene.game.ctx;
-    g.drawImage(this.canvasElement, this.offset.x, this.offset.y);
+    g.drawImage(this.canvasEl, this.offset.x, this.offset.y);
     return super.render();
+  }
+
+  splat() {
+    let g = this.canvasCtx;
+    g.fillStyle = `hsl(${Math.round(Math.random() * 360)} , 50% , 50%)`;
+    g.beginPath();
+    g.arc(Math.random() * this.size.x, Math.random() * this.size.y, 128, 0, 2 * Math.PI);
+    g.fill();
+    this.scene.setAlarm(this.scene.game.frameRate * 10, this.splat.bind(this));
   }
 
 
